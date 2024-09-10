75b617063bcd6215b60f8ffde45c2fedc981765b
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-09 22:34:32 -0500

    feat(students): add student service client.

---
+++ src/app/learning/services/student.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import {BaseService} from "../../shared/services/base.service";
+import {Student} from "../model/student.entity";
+
+@Injectable({ providedIn: 'root' })
+export class StudentService extends BaseService<Student>{
+
+  constructor() {
+    super();
+    this.resourceEndpoint = 'students';
+  }
+}
