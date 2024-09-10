3088b52d274b2b4e5f182b73db6230f5842dcb95
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-09 22:34:58 -0500

    feat(students): add enrollment service client.

---
+++ src/app/learning/services/enrollment.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {Enrollment} from "../model/enrollment.entity";
+import {BaseService} from "../../shared/services/base.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EnrollmentService extends BaseService<Enrollment> {
+
+  constructor() {
+    super();
+    this.resourceEndpoint = 'enrollments';
+  }
+}
