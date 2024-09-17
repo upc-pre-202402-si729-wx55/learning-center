581138f7eb39cb8f5bb4f3859c45b24a8038e1b0
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-16 21:45:55 -0500

    feat(in-app-navigation): setup in-app navigation rules.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./public/pages/home/home.component";
+import {AboutComponent} from "./public/pages/about/about.component";
+import {CourseManagementComponent} from "./learning/pages/course-management/course-management.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'learning/courses', component: CourseManagementComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full'}
+];
