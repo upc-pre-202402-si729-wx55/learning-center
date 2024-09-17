002d882bcc558c2eef9bd22b770d7e6d4067cf46
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-16 21:54:10 -0500

    feat(page-not-found): register navigation rule for page not found component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from "./public/pages/home/home.component";
 import {AboutComponent} from "./public/pages/about/about.component";
 import {CourseManagementComponent} from "./learning/pages/course-management/course-management.component";
+import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent},
-  { path: 'about', component: AboutComponent},
+  { path: 'home',             component: HomeComponent},
+  { path: 'about',            component: AboutComponent},
   { path: 'learning/courses', component: CourseManagementComponent},
-  { path: '', redirectTo: 'home', pathMatch: 'full'}
+  { path: '',                 redirectTo: 'home', pathMatch: 'full'},
+  { path: '**',               component: PageNotFoundComponent}
 ];
