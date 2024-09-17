eaf4dd30f225c62e84de0ec1d6ab4d9ac0129290
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-16 21:59:19 -0500

    feat(main): implement main structure and behavior.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatAnchor} from "@angular/material/button";
+import {LanguageSwitcherComponent} from "./public/components/language-switcher/language-switcher.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatAnchor, RouterLink, LanguageSwitcherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'learning-center';
+  protected options = [
+    { path: '/home', title: 'Home'},
+    { path: '/learning/courses', title: 'Courses'},
+    { path: '/about', title: 'About'}
+  ]
 }
