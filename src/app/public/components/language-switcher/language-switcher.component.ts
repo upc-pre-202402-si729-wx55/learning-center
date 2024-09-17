ebaf27b78046fbe9505443b2f0db9e9aebf136a8
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-16 21:38:49 -0500

    feat(i18n): add language switcher component.

---
+++ src/app/public/components/language-switcher/language-switcher.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
+import {MatButtonToggle, MatButtonToggleGroup} from "@angular/material/button-toggle";
+
+@Component({
+  selector: 'app-language-switcher',
+  standalone: true,
+  imports: [
+    MatButtonToggleGroup,
+    MatButtonToggle
+  ],
+  templateUrl: './language-switcher.component.html',
+  styleUrl: './language-switcher.component.css'
+})
+export class LanguageSwitcherComponent {
+  protected currentLang: string = 'en';
+  protected languages: string[] = ['en', 'es'];
+
+  constructor(private translate: TranslateService) {
+    this.currentLang = this.translate.currentLang;
+  }
+
+  useLanguage(language: string) {
+    this.translate.use(language);
+    this.currentLang = language;
+  }
+}
