f14ecb033e397cd42818605dc90b1fa419a2609a
Angel Velasquez <pcisavel@upc.edu.pe>
2024-11-13 11:48:37 -0500

    chore: replace base path for web service.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  serverBasePath: 'http://localhost:3000/api/v1'
+  //serverBasePath: 'http://localhost:3000/api/v1'
+  serverBasePath: 'http://localhost:8080/api/v1'
 };
