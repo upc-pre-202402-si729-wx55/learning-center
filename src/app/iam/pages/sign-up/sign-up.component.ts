e972ecfb4ea81aa3f230fd7614bb27b22b231732
Angel Velasquez <pcisavel@upc.edu.pe>
2024-11-13 11:35:05 -0500

    feat(iam): add sign-up component.

---
+++ src/app/iam/pages/sign-up/sign-up.component.ts
@@ -0,0 +1,64 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {AuthenticationService} from "../../services/authentication.service";
+import {BaseFormComponent} from "../../../shared/components/base-form.component";
+import {SignUpRequest} from "../../model/sign-up.request";
+import {MatButton} from "@angular/material/button";
+import {MatCard, MatCardContent, MatCardHeader} from "@angular/material/card";
+import {MatFormField} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+
+/**
+ * Sign up component
+ */
+@Component({
+  selector: 'app-sign-up',
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatButton,
+    MatCard,
+    MatCardContent,
+    MatCardHeader,
+    MatFormField,
+    MatInput,
+    ReactiveFormsModule
+  ],
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.css'
+})
+export class SignUpComponent extends BaseFormComponent implements OnInit {
+  form!: FormGroup;
+  submitted: boolean = false;
+
+  /**
+   * Constructor
+   * @param builder {@link FormBuilder} - form builder
+   * @param authenticationService {@link AuthenticationService} - authentication service
+   */
+  constructor(private builder: FormBuilder, private authenticationService: AuthenticationService) {
+    super();
+  }
+
+  /**
+   * On init Event Handler
+   */
+  ngOnInit(): void {
+    this.form = this.builder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  /**
+   * On submit Event Handler
+   */
+  onSubmit() {
+    if (this.form.invalid) return;
+    let username = this.form.value.username;
+    let password = this.form.value.password;
+    const signUnRequest = new SignUpRequest(username, password);
+    this.authenticationService.signUp(signUnRequest);
+    this.submitted = true;
+  }
+}
