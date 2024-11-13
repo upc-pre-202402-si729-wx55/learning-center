1185f993368f964c66c3096a2dd8982464384593
Angel Velasquez <pcisavel@upc.edu.pe>
2024-11-13 11:28:24 -0500

    feat(iam): add sign-in component.

---
+++ src/app/iam/pages/sign-in/sign-in.component.ts
@@ -0,0 +1,64 @@
+import {Component, OnInit} from '@angular/core';
+import {BaseFormComponent} from "../../../shared/components/base-form.component";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {AuthenticationService} from "../../services/authentication.service";
+import {SignInRequest} from "../../model/sign-in.request";
+import {MatCard, MatCardContent, MatCardHeader} from "@angular/material/card";
+import {MatFormField} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
+
+/**
+ * Sign in component
+ */
+@Component({
+  selector: 'app-sign-in',
+  standalone: true,
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatFormField,
+    ReactiveFormsModule,
+    MatInput,
+    MatButton
+  ],
+  templateUrl: './sign-in.component.html',
+  styleUrl: './sign-in.component.css'
+})
+export class SignInComponent extends BaseFormComponent implements OnInit {
+  form!: FormGroup;
+  submitted = false;
+
+  /**
+   * Constructor
+   * @param builder {@link FormBuilder} - form builder
+   * @param authenticationService {@link AuthenticationService} - authentication service
+   */
+  constructor(private builder: FormBuilder, private authenticationService: AuthenticationService) {
+    super();
+  }
+
+  /**
+   * On init Event Handler
+   */
+  ngOnInit(): void {
+    this.form = this.builder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  /**
+   * On submit Event Handler
+   */
+  onSubmit() {
+    if (this.form.invalid) return;
+    let username = this.form.value.username;
+    let password = this.form.value.password;
+    const signInRequest = new SignInRequest(username, password);
+    this.authenticationService.signIn(signInRequest);
+    this.submitted = true;
+  }
+
+}
