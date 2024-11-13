5dba1f05d23cb073db841c4116b37f0202e36092
Angel Velasquez <pcisavel@upc.edu.pe>
2024-11-13 11:28:09 -0500

    feat(iam): configure authentication interceptor.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,11 @@ import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
-import {HttpClient, provideHttpClient} from "@angular/common/http";
+import {HttpClient, provideHttpClient, withInterceptors} from "@angular/common/http";
 import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
 import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {authenticationInterceptor} from "./iam/services/authentication.interceptor";
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -14,7 +15,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authenticationInterceptor])),
     importProvidersFrom(TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
